13cf6b1674da5f402f2dfcd3d74c455ec23b7097
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2024-01-06 01:25:32 +0700

    first commit

---
+++ src/views/app.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-300">Hello world!</h1>
+      <a href="/test">testing</a>
+    </>
+  );
+}
